bc8fa39ec93840f4e2e59ac3f7bd00aa17e2c01c
cofi420 <filip.vidakovic.04@gmail.com>
2024-11-13 23:55:25 +0100

    Add basic navigation to main

--- app/src/main/java/com/example/eventplanner/activities/HomeActivity.java
+++ app/src/main/java/com/example/eventplanner/activities/HomeActivity.java
@@ -1,26 +1,109 @@
 package com.example.eventplanner.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.ui.NavigationUI;
 
 import com.example.eventplanner.R;
+import com.example.eventplanner.databinding.ActivityHomeBinding;
+import com.google.android.material.navigation.NavigationView;
+
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class HomeActivity extends AppCompatActivity {
+    private ActivityHomeBinding binding;
+    private AppBarConfiguration mAppBarConfiguration;
+    private DrawerLayout drawer;
+    private NavigationView navigationView;
+    private NavController navController;
+    private Toolbar toolbar;
+    private ActionBar actionBar;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+    private Set<Integer> topLevelDestinations = new HashSet<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_home);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        drawer = binding.drawerLayout;
+        navigationView = binding.navigationView;
+        setSupportActionBar(toolbar);
+        actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(false);
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_hamburger);
+            actionBar.setHomeButtonEnabled(false);
+        }
+        actionBarDrawerToggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        topLevelDestinations.add(R.id.nav_settings);
+        navController = Navigation.findNavController(this, R.id.fragment_nav_content_main);
+        navController.addOnDestinationChangedListener((navController, navDestination, bundle) -> {
+            int id = navDestination.getId();
+            boolean isTopLevelDestination = topLevelDestinations.contains(id);
+            if (!isTopLevelDestination) {
+                if (id == R.id.nav_home) {
+                    Toast.makeText(HomeActivity.this, "Home", Toast.LENGTH_SHORT).show();
+                } else if (id == R.id.nav_settings) {
+                    Toast.makeText(HomeActivity.this, "Settings", Toast.LENGTH_SHORT).show();
+                } else if (id == R.id.nav_logout) {
+                    Toast.makeText(HomeActivity.this, "Logout", Toast.LENGTH_SHORT).show();
+                }
+                drawer.closeDrawers();
+            } else {
+                if (id == R.id.nav_settings) {
+                    Toast.makeText(HomeActivity.this, "Settings", Toast.LENGTH_SHORT).show();
+                } else if (id == R.id.nav_language) {
+                    Toast.makeText(HomeActivity.this, "Language", Toast.LENGTH_SHORT).show();
+                }
+            }
         });
+        mAppBarConfiguration = new AppBarConfiguration
+                .Builder(R.id.nav_home, R.id.nav_logout, R.id.nav_settings, R.id.nav_language)
+                .setOpenableLayout(drawer)
+                .build();
+        NavigationUI.setupWithNavController(navigationView, navController);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        navController = Navigation.findNavController(this, R.id.fragment_nav_content_main);
+        return NavigationUI.onNavDestinationSelected(item, navController) || super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        navController = Navigation.findNavController(this, R.id.fragment_nav_content_main);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration) || super.onSupportNavigateUp();
     }
 }
