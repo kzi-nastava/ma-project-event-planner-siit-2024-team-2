5413f70fd424f5f924f02a549c29beb45088d818
Matija Šeremet <matija.sch03@gmail.com>
2024-11-17 01:51:08 +0100

    add service card improvements

--- app/src/main/java/com/example/eventplanner/model/Service.java
+++ app/src/main/java/com/example/eventplanner/model/Service.java
@@ -1,4 +1,49 @@
 package com.example.eventplanner.model;
 
 public class Service {
+    private Long id;
+    private String title;
+    private String description;
+    private int image;
+
+    public Service(Long id, String title, String description, int image) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.image = image;
+    }
+
+    public Service() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
 }
