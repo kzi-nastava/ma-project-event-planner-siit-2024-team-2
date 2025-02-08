a9f28859fcbfadad1d83cd4644354053ded5b4fc
FilipT03 <filipsombor@gmail.com>
2025-02-08 23:58:59 +0100

    Add event adapter for recycler view

---
+++ app/src/main/java/com/example/eventplanner/adapters/EventAdapter.java
@@ -0,0 +1,99 @@
+package com.example.eventplanner.adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.eventplanner.R;
+import com.example.eventplanner.dto.event.EventSummaryDto;
+import com.example.eventplanner.model.event.Event;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import lombok.Getter;
+
+public class EventAdapter extends RecyclerView.Adapter<EventAdapter.ViewHolder> {
+    private List<EventSummaryDto> localDataSet = new ArrayList<>();
+
+    /**
+     * Provide a reference to the type of views that you are using
+     * (custom ViewHolder)
+     */
+    @Getter
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private final TextView eoUsername;
+        private final TextView eoEmail;
+        private final TextView eventName;
+        private final TextView eventDate;
+        private final TextView eventDescription;
+        private final Button moreInfo;
+        private final ImageButton heart;
+
+        public ViewHolder(View view) {
+            super(view);
+            // Define click listener for the ViewHolder's View
+
+            eoUsername = (TextView) view.findViewById(R.id.text_eo_username);
+            eoEmail = (TextView) view.findViewById(R.id.text_eo_email);
+            eventName = (TextView) view.findViewById(R.id.text_event_name);
+            eventDate = (TextView) view.findViewById(R.id.text_event_date);
+            eventDescription = (TextView) view.findViewById(R.id.text_event_description);
+            moreInfo = (Button) view.findViewById(R.id.btn_event_more_info);
+            heart = (ImageButton) view.findViewById(R.id.btn_event_heart);
+        }
+
+    }
+
+    /**
+     * Initialize the dataset of the Adapter
+     *
+     * @param dataSet String[] containing the data to populate views to be used
+     * by RecyclerView
+     */
+    public EventAdapter(List<EventSummaryDto> dataSet) {
+        localDataSet = dataSet;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        // Create a new view, which defines the UI of the list item
+        View view = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.fragment_event_compact, viewGroup, false);
+
+        return new ViewHolder(view);
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, final int position) {
+
+        // Get element from your dataset at this position and replace the
+        // contents of the view with that element
+//        viewHolder.getEoUsername().setText(localDataSet[position].getCreatorUsername());
+//        viewHolder.getEoEmail().setText(localDataSet[position].getCreatorEmail());
+        viewHolder.getEventName().setText(localDataSet.get(position).getName());
+        viewHolder.getEventDate().setText(
+                LocalDateTime.ofInstant(Instant.ofEpochMilli(localDataSet.get(position).getDate()),
+                                        ZoneId.systemDefault()).toString());
+        viewHolder.getEventDescription().setText(localDataSet.get(position).getDescription());
+        viewHolder.getEventDescription().setText(localDataSet.get(position).getDescription());
+
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return localDataSet.size();
+    }
+}
