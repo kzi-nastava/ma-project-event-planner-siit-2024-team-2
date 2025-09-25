3ce0cd11e7fbcee641a63911b53062a3d0821a1b
FilipT03 <filipsombor@gmail.com>
2025-09-25 14:32:57 +0200

    Update Home page to use view model for data

---
+++ app/src/main/java/com/example/eventplanner/fragments/home/HomeViewModel.java
@@ -0,0 +1,32 @@
+package com.example.eventplanner.fragments.home;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.example.eventplanner.clients.repositories.EventRepository;
+import com.example.eventplanner.clients.repositories.serviceproduct.ServiceProductRepository;
+import com.example.eventplanner.dto.event.EventSummaryDto;
+import com.example.eventplanner.dto.serviceproduct.ServiceProductSummaryDto;
+import com.example.eventplanner.utils.ObserverTracker;
+
+import java.util.List;
+
+import lombok.Getter;
+
+public class HomeViewModel extends ViewModel {
+    private final EventRepository eventRepository = new EventRepository();
+    private final ServiceProductRepository serviceProductRepository = new ServiceProductRepository();
+
+    @Getter
+    private final MutableLiveData<List<EventSummaryDto>> topEvents = new MutableLiveData<>();
+    @Getter
+    private final MutableLiveData<List<ServiceProductSummaryDto>> topServiceProducts = new MutableLiveData<>();
+    private final ObserverTracker tracker = new ObserverTracker();
+
+    public void fetchTopEvents() {
+        tracker.observeOnce(eventRepository.getTop5(), topEvents, true);
+    }
+    public void fetchTopServiceProducts() {
+        tracker.observeOnce(serviceProductRepository.getTop5(), topServiceProducts, true);
+    }
+}
